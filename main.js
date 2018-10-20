b6fe20fc30f0771a018c4862f44ed84a4946410c
‘Brittany <brittany.stubbs1827@gmail.com>
2018-10-20 13:45:35 -0400

    added 4 pop up windows and console log

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-let age;
-console.log("age is currently: " + age);
-age = 26;
-console.log("age is currently: " + age)
-age = 26 + 1;
+let userName = prompt("What is your name?");
+
+let nameConfirm = confirm("Just checking, is " + userName + " your real name?");
+
+if(nameConfirm) {
+    alert("Welcome to my page, " + username + "!");
+}
+else {
+    userName = prompt("Okay, what is your name?");
+}
+
+console.log("The user's name is " + userName + ".")
